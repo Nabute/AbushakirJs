29fb0471e54b2b118070264a504aa40f294e518e
Nabute <nabute925@gmail.com>
2020-09-11 19:37:13 +0300

    feat(NUMBER): to ethiopiac number convertor

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+import EtDatetime from './Abushakir/datetime';
+import BahireHasab from './Abushakir/bh';
+import ETC from './Abushakir/etc';
+import Duration from './utils/duration';
+export { EtDatetime, BahireHasab, ETC, Duration };
---
+++ lib/utils/Utils.d.ts
@@ -0,0 +1,4 @@
+declare namespace Utility {
+  interface Duration {}
+  class Constants {}
+}
